07b8f6416f80d3f1cf82ae61111fcf2a994eaf89
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-06-09 21:48:17 -0600

    Create HelloUser.vb

---
+++ HelloUser.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim name As String
+
+        Console.Write("Enter your name: ")
+        name = Console.ReadLine()
+
+        Console.WriteLine("Hello, " & name & "! Welcome to Visual Basic!")
+
+        Console.ReadLine()
+    End Sub
+End Module
